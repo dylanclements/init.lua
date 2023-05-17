879ea6f1812fdaa36806f27ba5a7ff1cf1ce6a2c
Dylan Clements <dylan.clements99@gmail.com>
2023-05-17 21:22:09 +1000

    initial commit

---
+++ init.lua
@@ -0,0 +1,10 @@
+require("dylanclements")
+
+vim.opt.expandtab = true    -- Use spaces instead of tabs
+vim.opt.tabstop = 4         -- Number of spaces a tab in the file counts for
+vim.opt.shiftwidth = 4      -- Number of spaces to use for each step of (auto)indent
+vim.opt.softtabstop = 4     -- Number of spaces that a <Tab> or <BS> counts for while performing editing operations like insert, delete and join
+
+vim.opt.number = true          -- Enable absolute line numbers
+vim.opt.relativenumber = true  -- Enable relative line numbers
+
---
+++ lua/dylanclements/init.lua
@@ -0,0 +1,1 @@
+require("dylanclements.remap")
