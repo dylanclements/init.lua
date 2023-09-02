eeb9de210c80bfcf347144fefb547fc04b4825b3
Dylan Clements <dylan.clements99@gmail.com>
2023-09-02 17:31:10 +1000

    whats this

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -79,11 +79,6 @@ _G.packer_plugins = {
     path = "/Users/dylanclements/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
-  ["cellular-automaton.nvim"] = {
-    loaded = true,
-    path = "/Users/dylanclements/.local/share/nvim/site/pack/packer/start/cellular-automaton.nvim",
-    url = "https://github.com/eandrju/cellular-automaton.nvim"
-  },
   ["cmp-nvim-lsp"] = {
     loaded = true,
     path = "/Users/dylanclements/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
