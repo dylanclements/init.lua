b23f2e5bc4254cc73750960374293c3531c320e3
Dylan Clements <dylanc@canva.com>
2024-05-21 16:22:31 +1000

    add nvim tree

--- lua/dylanclements/packer.lua
+++ lua/dylanclements/packer.lua
@@ -9,8 +9,7 @@ return require('packer').startup(function(use)
 
     -- fuzzy finder
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.1',
-        -- or                            , branch = '0.1.x',
+        'nvim-telescope/telescope.nvim', branch = '0.1.x',
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
 
@@ -62,5 +61,15 @@ return require('packer').startup(function(use)
     use 'eandrju/cellular-automaton.nvim'
 
     -- use 'github/copilot.vim'
-    -- use 'mfussenegger/nvim-jdtls'
+
+    -- java developer tools
+    use 'mfussenegger/nvim-jdtls'
+
+    -- nvim tree
+    use {
+        'nvim-tree/nvim-tree.lua',
+        requires = {
+            'nvim-tree/nvim-web-devicons', -- optional
+        },
+    }
 end)
