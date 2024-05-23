db74b0a68d1565f40818aa90af8deccf52bfc1fb
Dylan Clements <dylanc@canva.com>
2024-05-24 09:31:17 +1000

    clean up nvim tree stuff

--- after/plugin/tree.lua
+++ after/plugin/tree.lua
@@ -2,21 +2,13 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- optionally enable 24-bit colour
--- already set to true in set.lua but its here in case that changes
--- vim.opt.termguicolors = true
-
--- empty setup using defaults
-require("nvim-tree").setup()
-
--- OR setup with some options
 require("nvim-tree").setup({
   sort = {
     sorter = "case_sensitive",
   },
   view = {
     width = {
-        min = 30
+      min = 30,
     }
   },
   renderer = {
