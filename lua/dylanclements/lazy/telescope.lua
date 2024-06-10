e5ff82c14ac8355a8c76828ccfc1e2d3cc6e7df1
Dylan Clements <dylan.clements99@gmail.com>
2024-06-10 18:47:16 +1000

    put plenary dependency in telescope

--- lua/dylanclements/lazy/telescope.lua
+++ lua/dylanclements/lazy/telescope.lua
@@ -1,6 +1,7 @@
 return {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.6",
+    dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
         require('telescope').setup({})
 
@@ -11,14 +12,16 @@ return {
             local word = vim.fn.expand("<cword>")
             builtin.grep_string({ search = word })
         end)
+
         vim.keymap.set('n', '<leader>pWs', function()
             local word = vim.fn.expand("<cWORD>")
             builtin.grep_string({ search = word })
         end)
+
         vim.keymap.set('n', '<leader>ps', function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
         end)
-        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+
+        -- vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
     end
 }
-
