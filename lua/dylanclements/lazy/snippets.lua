581bee533c683bb0c3a6dc9b79e138ff9a9a5c4d
Dylan Clements <dylan.clements99@gmail.com>
2024-09-08 19:39:51 +1000

    update snippets

--- lua/dylanclements/lazy/snippets.lua
+++ lua/dylanclements/lazy/snippets.lua
@@ -1,8 +1,7 @@
 return {
 	"L3MON4D3/LuaSnip",
-	version = "v2.*",
-	build = "make install_jsregexp",
-     dependencies = { "rafamadriz/friendly-snippets" },
-    config = function()
-    end
+	-- follow latest release.
+	version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+	-- install jsregexp (optional!).
+	build = "make install_jsregexp"
 }
