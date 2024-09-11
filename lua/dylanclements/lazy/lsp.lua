82e5e6dd8dcfb4641602cff4a61e54a2f5da251c
Dylan Clements <dylan.clements99@gmail.com>
2024-09-11 21:29:28 +1000

    Add jdtls

--- lua/dylanclements/lazy/lsp.lua
+++ lua/dylanclements/lazy/lsp.lua
@@ -34,6 +34,7 @@ return {
                 "sqlls",
                 "dockerls",
                 "nil_ls",
+                'jdtls',
             },
             handlers = {
                 function(server_name) -- default handler (optional)
