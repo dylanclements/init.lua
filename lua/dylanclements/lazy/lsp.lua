d9acf8e6d1ba6fa2586a5fd335a371ff9e149d03
Dylan Clements <dylanc@canva.com>
2024-09-15 17:37:35 +1000

    get jdtls to work

--- lua/dylanclements/lazy/lsp.lua
+++ lua/dylanclements/lazy/lsp.lua
@@ -10,7 +10,84 @@ return {
         "hrsh7th/nvim-cmp",
         "L3MON4D3/LuaSnip",
         "saadparwaiz1/cmp_luasnip",
-        "j-hui/fidget.nvim"
+        "j-hui/fidget.nvim",
+        "mfussenegger/nvim-jdtls",
+    },
+    opts = {
+        setup = {
+            jdtls = function(_, opts)
+                vim.api.nvim_create_autocmd("FileType", {
+                    pattern = "java",
+                    callback = function()
+                        local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+                        -- vim.lsp.set_log_level('DEBUG')
+                        local workspace_dir = "/User/dylanc/work/" ..
+                            project_name -- See `:help vim.lsp.start_client` for an overview of the supported `config` options.
+                        local config = {
+                            -- The command that starts the language server
+                            -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
+                            cmd = {
+
+                                "java", -- or '/path/to/java17_or_newer/bin/java'
+                                -- depends on if `java` is in your $PATH env variable and if it points to the right version.
+
+                                "-javaagent:/home/jake/.local/share/java/lombok.jar",
+                                -- '-Xbootclasspath/a:/home/jake/.local/share/java/lombok.jar',
+                                "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+                                "-Dosgi.bundles.defaultStartLevel=4",
+                                "-Declipse.product=org.eclipse.jdt.ls.core.product",
+                                "-Dlog.protocol=true",
+                                "-Dlog.level=ALL",
+                                -- '-noverify',
+                                "-Xms64g",
+                                "--add-modules=ALL-SYSTEM",
+                                "--add-opens",
+                                "java.base/java.util=ALL-UNNAMED",
+                                "--add-opens",
+                                "java.base/java.lang=ALL-UNNAMED",
+                                "-jar",
+                                vim.fn.glob(
+                                    "/Users/dylanc/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher.cocoa.macosx.aarch64_1.2.1100.v20240722-2106.jar")
+                                -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                                       ^^^^^^^^^^^^^^
+                                -- Must point to the                                                     Change this to
+                                -- eclipse.jdt.ls installation                                           the actual version
+
+                                    "-configuration",
+                                "/Users/dylanc/.local/share/nvim/mason/packages/jdtls/config_mac_arm",
+                                -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        ^^^^^^
+                                -- Must point to the                      Change to one of `linux`, `win` or `mac`
+                                -- eclipse.jdt.ls installation            Depending on your system.
+
+                                -- See `data directory configuration` section in the README
+                                "-data",
+                                workspace_dir,
+                            },
+
+                            -- This is the default if not provided, you can remove it. Or adjust as needed.
+                            -- One dedicated LSP server & client will be started per unique root_dir
+                            root_dir = require("jdtls.setup").find_root({ ".git", "WORKSPACE" }),
+
+                            -- Here you can configure eclipse.jdt.ls specific settings
+                            -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+                            -- for a list of options
+                            settings = {
+                                java = {},
+                            },
+                            handlers = {
+                                ["language/status"] = function(_, result)
+                                    -- print(result)
+                                end,
+                                ["$/progress"] = function(_, result, ctx)
+                                    -- disable progress updates.
+                                end,
+                            },
+                        }
+                        require("jdtls").start_or_attach(config)
+                    end,
+                })
+                return true
+            end,
+        },
     },
 
     config = function()
