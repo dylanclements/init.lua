5835712ba62d5043895b83e7053e7959132bdc14
Dylan Clements <dylanc@canva.com>
2024-09-30 17:08:21 +1000

    move jdtls autocmd out

--- lua/dylanclements/autocmds.lua
+++ lua/dylanclements/autocmds.lua
@@ -88,6 +88,7 @@ autocmd("FileType", {
 
 autocmd("FileType", {
     pattern = {
+        "java",
         "javascript",
         "javascriptreact",
         "typescript",
@@ -102,5 +103,78 @@ autocmd("FileType", {
         vim.opt_local.shiftwidth = 2
         vim.opt_local.tabstop = 2
         vim.opt_local.softtabstop = 2
+    end,
+})
+
+
+-- Spin up JDTLS if we are in a java file
+autocmd("FileType", {
+    group = DylanClementsGroup,
+    pattern = "java",
+    callback = function()
+        local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+        -- vim.lsp.set_log_level('DEBUG')
+
+        local home_dir = "/User/dylanc/"
+        local workspace_dir = home_dir .. "work/" .. project_name
+        local local_share_dir = home_dir .. ".local/share/"
+
+        -- See `:help vim.lsp.start_client` for an overview of the supported `config` options.
+        require("jdtls").start_or_attach({
+            -- The command that starts the language server
+            -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
+            cmd = {
+                "java", -- or '/path/to/java17_or_newer/bin/java'
+                -- depends on if `java` is in your $PATH env variable and if it points to the right version.
+
+                -- "-javaagent:local_share_dir .. java/lombok.jar",
+                -- '-Xbootclasspath/a:local_share_dir .. java/lombok.jar',
+                "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+                "-Dosgi.bundles.defaultStartLevel=4",
+                "-Declipse.product=org.eclipse.jdt.ls.core.product",
+                "-Dlog.protocol=true",
+                "-Dlog.level=ALL",
+                -- '-noverify',
+                "-Xms32g",
+                "-Xmx64g",
+                "--add-modules=ALL-SYSTEM",
+                "--add-opens",
+                "java.base/java.util=ALL-UNNAMED",
+                "--add-opens",
+                "java.base/java.lang=ALL-UNNAMED",
+                "-jar",
+                vim.fn.glob(
+                    local_share_dir ..
+                    "nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher.cocoa.macosx.aarch64_1.2.1100.v20240722-2106.jar")
+                -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                                       ^^^^^^^^^^^^^^
+                -- Must point to the                                                     Change this to
+                -- eclipse.jdt.ls installation                                           the actual version
+
+                    "-configuration",
+                local_share_dir .. "nvim/mason/packages/jdtls/config_mac_arm",
+                -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        ^^^^^^
+                -- Must point to the                      Change to one of `linux`, `win` or `mac`
+                -- eclipse.jdt.ls installation            Depending on your system.
+
+                -- See `data directory configuration` section in the README
+                "-data",
+                workspace_dir,
+            },
+
+            -- This is the default if not provided, you can remove it. Or adjust as needed.
+            -- One dedicated LSP server & client will be started per unique root_dir
+            root_dir = require("jdtls.setup").find_root({ ".git", "WORKSPACE" }),
+
+            -- Here you can configure eclipse.jdt.ls specific settings
+            -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+            -- for a list of options
+            settings = {
+                java = {},
+            },
+
+            -- add callbacks for events
+            handlers = {
+            },
+        })
     end,
 })
