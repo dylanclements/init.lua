65a03cd8deb65870824b0455653971b1bc5326dd
Dylan Clements <dylan.clements99@gmail.com>
2025-07-13 12:00:35 +1000

    intellij background

--- lua/dylanclements/lazy/colors.lua
+++ lua/dylanclements/lazy/colors.lua
@@ -1,9 +1,6 @@
 function ColorMyPencils(color)
 	color = color or "jb"
 	vim.cmd.colorscheme(color)
-
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 
@@ -80,8 +77,49 @@ return {
         priority = 1000,
         opts = {},
         config = function()
-            -- require("jb").setup({transparent = true}) -- enable if you want transparency
+            -- Configure jb.nvim with custom background
+            require("jb").setup({
+                transparent = false, -- Disable transparency to get solid background
+                disable_hl_args = {
+                    bold = false,
+                    italic = false,
+                },
+                snacks = {
+                    explorer = {
+                        enabled = true,
+                    },
+                },
+            })
+            
+            -- Apply the colorscheme
             ColorMyPencils("jb")
+            
+            -- Override specific highlight groups for IntelliJ-like grey background
+            local grey_bg = "#2b2b2b"  -- IntelliJ-like grey
+            local float_bg = "#3c3f41" -- Slightly lighter grey for floating windows
+            
+            -- Set background for main areas
+            vim.api.nvim_set_hl(0, "Normal", { bg = grey_bg })
+            vim.api.nvim_set_hl(0, "NormalFloat", { bg = float_bg })
+            
+            -- Set background for inactive buffers and windows
+            vim.api.nvim_set_hl(0, "NonText", { bg = grey_bg })
+            vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = grey_bg })
+            vim.api.nvim_set_hl(0, "SignColumn", { bg = grey_bg })
+            vim.api.nvim_set_hl(0, "LineNr", { bg = grey_bg })
+            vim.api.nvim_set_hl(0, "CursorLine", { bg = "#323232" }) -- Slightly lighter for cursor line
+            vim.api.nvim_set_hl(0, "CursorLineNr", { bg = "#323232" })
+            
+            -- Set background for status line and tab line
+            vim.api.nvim_set_hl(0, "StatusLine", { bg = "#3c3f41" })
+            vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "#2b2b2b" })
+            vim.api.nvim_set_hl(0, "TabLine", { bg = "#3c3f41" })
+            vim.api.nvim_set_hl(0, "TabLineFill", { bg = "#2b2b2b" })
+            
+            -- Set background for sidebar and tree
+            vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = grey_bg })
+            vim.api.nvim_set_hl(0, "NvimTreeEndOfBuffer", { bg = grey_bg })
+            vim.api.nvim_set_hl(0, "NvimTreeStatusLine", { bg = grey_bg })
         end,
     },
 }
